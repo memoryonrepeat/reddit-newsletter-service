d945438e5c3035cd8392ce53af18dce52ab6425e
Thanh Nguyen <thanh.nguyen@frontiercargroup.com>
2020-09-29 23:38:01 +0200

    WIP cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { RedisModule } from 'nestjs-redis'
+import { ScheduleModule } from '@nestjs/schedule'
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     RedisModule.register({
-      host: 'redis',
-      onClientReady: async (client) => {
-        const stream = client.scanStream()
-        stream.on('data', async (data) => {
-          console.log('--->', data)
-          const res = await client.hgetall(data)
-          console.log('????', res)
-        })
-        /*const res = await client.scan(0, 'match', '*')
-        console.log('full db', res)*/
-      }
+      host: 'redis'
     })
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,45 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleInit } from '@nestjs/common'
 import { PreferenceInput } from './dtos/preference-input.dto'
 import { RedisService } from 'nestjs-redis'
+import { SchedulerRegistry } from '@nestjs/schedule'
+import { CronJob } from 'cron'
+
 
 @Injectable()
-export class AppService {
-  constructor(private readonly redisService: RedisService) {}
+export class AppService implements OnModuleInit {
+  constructor(
+  	private readonly redisService: RedisService,
+    private client,
+    private schedulerRegistry: SchedulerRegistry
+  ) {
+  	this.init()
+  }
+
+  // Workaround to use async constructor
+  async init(){
+  	this.client = await this.redisService.getClient()
+
+  }
+
+  // Lifecycle event
+  async onModuleInit() {
+  	const stream = this.client.scanStream()
+    stream.on('data', async (data) => {
+      const res = await this.client.hgetall(data)
+      console.log('--->', data, res)
+      const seconds = 5
+      const job = new CronJob(`${seconds} * * * * *`, () => {
+        console.log(`time (${seconds}) for job news to run!`);
+      })
+      this.schedulerRegistry.addCronJob('news', job)
+      
+    })
+    console.log(`The module has been initialized.`);
+  }
+
   async upsertPreference(preferenceInput: PreferenceInput) {
-    const client = await this.redisService.getClient()
     const { email, ...rest } = preferenceInput
-    await client.hmset(email, new Map(Object.entries(rest)))
+    await this.client.hmset(email, new Map(Object.entries(rest)))
     return preferenceInput
   }
 }
